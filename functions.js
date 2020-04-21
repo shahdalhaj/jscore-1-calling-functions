d58e8245de51cd3e46efe26c53c72ee0cd35556f
shahdalhaj <61591526+shahdalhaj@users.noreply.github.com>
2020-04-22 00:15:00 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,9 +1,17 @@
-function calculateAge(birthYear){
-    let currentYear = new date().getFullYear();
+function calculateAge(birthYear ) {
+    const currentYear = 2020;
+    let yourBirthYear = birthYear;
+     return (currentYear - yourBirthYear);
 
-    return currentYear - birthYear;
+
+ return (currentYear - birthYear);
 }
 
-function nameAndAge (name ,birthYear){
-    const 
+                                                       
+
+function fullInfo (name , birthYear) {
+    const getAge = calculateAge(birthYear);
+    let anyName = name;
+    const message = "My name is " + anyName + "and i'm " + getAge + "years old";
 }
+console.log(fullInfo (Shahd,26));
