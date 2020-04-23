a8b6891e173c394249103c8d7b743156eeaeae4b
shahdalhaj <61591526+shahdalhaj@users.noreply.github.com>
2020-04-23 14:00:54 +0100

    fixed the function / done

--- functions.js
+++ functions.js
@@ -1,17 +1,17 @@
 function calculateAge(birthYear ) {
-    const currentYear = 2020;
+
     let yourBirthYear = birthYear;
-     return (currentYear - yourBirthYear);
+     return (yourBirthYear);
 
 
- return (currentYear - birthYear);
+ 
 }
 
                                                        
 
 function fullInfo (name , birthYear) {
     const getAge = calculateAge(birthYear);
-    let anyName = name;
-    const message = "My name is " + anyName + "and i'm " + getAge + "years old";
+    const message = "Hello, my name is " + name + " and i'm " + getAge + " years old";
+    return message;
 }
-console.log(fullInfo (Shahd,26));
+console.log(fullInfo("Shahd", 26));
